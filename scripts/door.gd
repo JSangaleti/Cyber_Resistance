c9ceb8b8b1e0d9f070bce0f265995eddf68d3cc4
mvpolido <mariavitoriapsmv@gmail.com>
2024-05-23 18:12:33 -0300

    Init
    Inserção do nó Player no grupo Player;
    criação de um script door para a transição de cenas (ambiente externo do mundo para ambiente interno da cafeteria);
    o personagem ainda não fica na posição correta ao realizar a transição de cenas.

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,13 +1,23 @@
-extends Node2D
+extends Area2D
 
+#Variável para atribuir o caminho da próxima cena. Contribuirá para que o código seja reutilizado de forma mais simples
+@export var nextScene : String = ""
 
+#Variável booleana para recer se o player está ou não na área2D para a transição.
+#Essa variável está como global nesse script para que seja usada em mais de uma função.
+var player_on_door : bool = false
 
-func _change_scene_to(next_scene_path: String) -> void:
-	get_tree().change_scene_to_file(next_scene_path)
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _on_body_entered(body):
+# Verificando se o body está no grupo Player. 
+# Na cena "player", incluí o mesmo em um grupo denominado Player. 
+	if body.is_in_group("Player"):
+		player_on_door = true
+
+func _on_body_exited(body):
+	player_on_door = false
+
 func _process(delta):
-	pass
-
+	if player_on_door and Input.is_action_just_pressed("Enter"):
+		get_tree().change_scene_to_file(nextScene)
+		
+		
