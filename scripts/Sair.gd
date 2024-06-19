7a4643e483fdb6bd9f5f2368185cabfcb5feb437
mvpolido <mariavitoriapsmv@gmail.com>
2024-06-19 12:54:21 -0300

    Init
    Correção de erro que estava ocorrendo ao sair da cena do computador.

--- scripts/Sair.gd
+++ scripts/Sair.gd
@@ -8,8 +8,14 @@ extends Area2D
 #Essa variável está como global nesse script para que seja usada em mais de uma função.
 
 func _process(delta):
-	if Input.is_action_just_pressed("Esc"):
-		print("Dentro do if, ou seja, Enter e player on door funcionando")
+#		Comparo com o caminho para que cada tecla realmente execute apenas sua função, de sair ou entrar na cena do pc.
+#		Sem tal comparação, tanto a tecla Esc quanto a tecla E (ação) podem ser usadas para sair ou entrar na cena. 
+	if Input.is_action_just_pressed("Esc") and nextScene != "res://scenes/computador.tscn":
+		print("Dentro do if, ou seja, Esc e player on door funcionando")
 		get_tree().change_scene_to_file(nextScene)
 		print(nextScene)
+	elif Input.is_action_just_pressed("ação") and nextScene == "res://scenes/computador.tscn":
+		get_tree().change_scene_to_file(nextScene)
+		print(nextScene)
+		
 		
--- scripts/positions.gd
+++ scripts/positions.gd
@@ -3,10 +3,17 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-#Os comandos seguintes são para o ajuste de posição do personagem ao passar por uma transição de cenas.
+		#Os comandos seguintes são para o ajuste de posição do personagem ao passar por uma transição de cenas.
 	if Global.from_scene != null:
-		$Player.set_position(get_node(Global.from_scene + "pos").position)
+		if(Global.from_scene == "Cafeteria"):
+			print("ignorando...")
+#			O erro é o seguinte: sem esse if, quando vai sair do computador e voltar pra cafeteria, o script Positions
+#			tenta encontrar o nó Cafeteriapos. Porém, dentro do pc não é necessário uma posição, visto que o player não
+#			entra dentro da cena. Contudo, criei um script chamado positionComputer, que faz apenas a mudança para a próxima cena,
+#			sem se preocupar com a posição do Player. 
+		else:
+			$Player.set_position(get_node(Global.from_scene + "pos").position)
+
 #------------------------------------------------------------------------------------------------------
 
 
