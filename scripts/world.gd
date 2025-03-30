846bd5a8b322316a19b620cbc9c49d575b02629e
mvpolido <mariavitoriapsmv@gmail.com>
2025-03-29 23:56:34 -0300

    Novo Cenário
    Criação de nova cena para ser o interior da universidade;
    Criação dos tiles iniciais: pisos, primeira sala, mesas, cadeiras, lousa (tudo apenas como protótipo).
    Os tiles ainda estão sendo desenhados/criados.

--- scripts/world.gd
+++ scripts/world.gd
@@ -5,8 +5,6 @@ func _ready() -> void:
 	Global.update_scene("World");
 	$Player.global_position = Global.update_position();
 	
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
